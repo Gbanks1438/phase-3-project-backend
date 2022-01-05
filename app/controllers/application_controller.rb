b9392e937fe15eec888ca57a84d12dd5a8d9b2c1
Gregory Banks <gbanks1438@gmail.com>
2022-01-04 21:21:06 -0500

    almost done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,18 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/" do
-    { message: "Congrats! You did a thing!" }.to_json
+    { message: "Ay-yo! You did a thing!" }.to_json
   end
 
   get "/guests" do
     Guest.all.to_json
   end
 
-  
+  ## BROKEN CODE
+  # get "/guests/:id" do
+  #   Guest.find(params[:id]).to_json
+  # end
+
   get "/hotels" do
     Hotel.all.to_json
   end
@@ -19,6 +23,19 @@ class ApplicationController < Sinatra::Base
   get "/reservations" do
     Reservation.all.to_json
   end
+
+  post "/reservations" do
+    Reservation.create(
+      {
+        hotel_id:params[:hotel_id], 
+        guest_id:params[:guest_id],
+        room_id:params[:room_id],
+        date_start:params[:date_start],
+        date_end:params[:date_end]
+      }).to_json
+  end
+
+  # DELETING RESERVATIONS
 
   
   get "/rooms" do
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,4 @@
 class Room < ActiveRecord::Base
-    #Macros go here
 
     has_many :reservations
     has_many :guests, through: :reservations
